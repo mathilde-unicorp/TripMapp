ef46e8573010f93c9d95a8bad8cacf06b0ed11fc
Mathilde RESSIER <mathilde@unicorp.dev>
2023-04-24 17:52:51 +0200

    Init project

---
+++ TripMapp/TripMappApp.swift
@@ -0,0 +1,20 @@
+//
+//  TripMappApp.swift
+//  TripMapp
+//
+//  Created by Ressier Mathilde on 24/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TripMappApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
