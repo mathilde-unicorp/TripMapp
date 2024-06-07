8575f42a79ac98762f1cbf9b54749764dcae4408
Mathilde RESSIER <mathilde@unicorp.dev>
2024-06-07 09:46:36 +0200

    Some rename and fixes on ViewModifiers

---
+++ TripMapp/Helpers/Views/ViewModifiers/View+clipToCircle.swift
@@ -0,0 +1,25 @@
+//
+//  ImageButton.swift
+//  TripMapp
+//
+//  Created by Ressier Mathilde on 15/05/2024.
+//
+
+import SwiftUI
+
+extension View {
+    func clipToCircle(backgroundColor: Color) -> some View {
+        return self
+            .padding()
+            .background(backgroundColor)
+            .clipShape(Circle())
+    }
+}
+
+#Preview {
+    VStack {
+        Image(systemName: "photo.fill")
+            .clipToCircle(backgroundColor: .blue)
+            .foregroundStyle(.white)
+    }
+}
--- TripMapp/Helpers/Views/ViewModifiers/View+onDeviceRotate.swift
+++ TripMapp/Helpers/Views/ViewModifiers/View+onDeviceRotate.swift
@@ -8,23 +8,25 @@
 import Foundation
 import SwiftUI
 
-// Our custom view modifier to track rotation and
-// call our action
+/// To track rotation and call an action when a rotation occurs
 struct DeviceRotationViewModifier: ViewModifier {
     let action: (UIDeviceOrientation) -> Void
 
     func body(content: Content) -> some View {
         content
             .onAppear()
-            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+            .onReceive(NotificationCenter.default.publisher(
+                for: UIDevice.orientationDidChangeNotification
+            )) { _ in
                 action(UIDevice.current.orientation)
             }
     }
 }
 
-// A View wrapper to make the modifier easier to use
 extension View {
-    func onDeviceRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+    func onDeviceRotate(
+        perform action: @escaping (UIDeviceOrientation) -> Void
+    ) -> some View {
         self.modifier(DeviceRotationViewModifier(action: action))
     }
 }
